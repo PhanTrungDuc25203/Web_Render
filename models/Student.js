ddccb83f1da65ec99bb7af2adf3c99e58a66b5b7
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-11-29 09:24:21 +0700

    change db

--- models/Student.js
+++ models/Student.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb+srv://pisceanphan25203:Q6egkOaXkCsJZExH@student.qjzzi.mongodb.net/student?retryWrites=true&w=majority', {
+const queryString = process.env.MONGODB_URI || "mongodb+srv://thanhdat2003pt:4Nhs3ch03m@cluster0.i8yo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//configure mongoose
+mongoose.connect(queryString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}).then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log('MongoDB connection error:', err.message));
 
 const StudentSchema = new Schema({
     studentId: Number,
