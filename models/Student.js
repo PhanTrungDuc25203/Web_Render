338a873aa2332cb842f2094f29cc45ba3764b070
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-11-29 09:16:03 +0700

    first commit

--- models/Student.js
+++ models/Student.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+mongoose.connect('mongodb+srv://pisceanphan25203:Q6egkOaXkCsJZExH@student.qjzzi.mongodb.net/student?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 const StudentSchema = new Schema({
     studentId: Number,
     name: String,
